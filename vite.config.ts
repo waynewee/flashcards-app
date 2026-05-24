8e18e6bbda3556d3ef742dd2e7a339b6ff0af709
Wayne Wee <wwayne.wee@gmail.com>
2026-05-24 22:03:59 +0800

    github

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const base =
+  process.env.GITHUB_ACTIONS === "true" && repositoryName
+    ? `/${repositoryName}/`
+    : "/";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [react()],
-})
+});
